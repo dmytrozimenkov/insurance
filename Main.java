09fc4c92dc9cb6cf3fe983ee830fa51f57557241
dmytrozimenkov <dizimka@icloud.com>
2017-05-20 01:13:10 +0300

    dima sosi huy

--- Main.java
+++ Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("1sfgfdgdfg11hui sdddos pidar i111");
+        primaryStage.setTitle("dima huy sosi shavka");
         primaryStage.setScene(new Scene(root, 800, 600));
         primaryStage.show();
     }
