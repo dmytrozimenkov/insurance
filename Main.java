a3be5bd8bd33a92273f23bfd877243ce4eaafd43
evgeniyparf <evgeniy.parf@gmail.com>
2017-05-20 01:09:28 +0300

    dima sosi huy

--- Main.java
+++ Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("hui sdddosi111");
+        primaryStage.setTitle("hui sdddos pidar i111");
         primaryStage.setScene(new Scene(root, 800, 600));
         primaryStage.show();
     }
