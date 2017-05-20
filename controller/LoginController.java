34e742ec467990cc7c03683d6a1b9805266d4a4f
dmytrozimenkov <dizimka@icloud.com>
2017-05-20 03:35:47 +0300

    1

--- controller/LoginController.java
+++ controller/LoginController.java
@@ -4,6 +4,10 @@ import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.scene.control.TextField;
+import sample.DB;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 
 public class LoginController {
@@ -14,6 +18,17 @@ public class LoginController {
     private TextField password;
 
     public void login_func()  {
-        System.out.println("hui");
+        DB db = new DB();
+        db.openConnection();
+        ResultSet rs = db.query("SELECT id_agent, password FROM Agent WHERE id_agent = '" + login.getText() + "' AND password = '" + password.getText() + "'");
+        try{
+            while(rs.next()){
+                System.out.println(rs.getInt(1));
+                System.out.println(rs.getString(2));
+            }
+        } catch (SQLException ex){
+            ex.printStackTrace();
+        }
+        db.closeConnection();
     }
 }
