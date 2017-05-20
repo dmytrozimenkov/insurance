76f50c3d5a77c446c772cd6faea092bd10e5ea1b
dmytrozimenkov <dizimka@icloud.com>
2017-05-20 03:20:41 +0300

    1

--- DB.java
+++ DB.java
@@ -1,5 +1,6 @@
 package sample;
 
+import javax.xml.transform.Result;
 import java.sql.*;
 
 /**
@@ -10,18 +11,31 @@ public class DB {
     private Connection  con;
     private Statement   stmt;
 
-    private static final String DB_URL     = "1";
-    private static final String DB_USER    = "";
+    private static final String DB_URL     = "jdbc:mysql://localhost:3306/insurance";
+    private static final String DB_USER    = "root";
     private static final String DB_PASS    = "";
 
     private boolean connection = false;
 
+    public static void main(String[] args) throws SQLException{
+        DB db = new DB();
+        db.openConnection();
+        ResultSet rsTest = db.query("SELECT * FROM agent");
+        while(rsTest.next()){
+            System.out.println(rsTest. getInt(1));
+            System.out.println(rsTest.getString(2));
+            System.out.println(rsTest.getString(3));
+        }
+        rsTest.close();
+        db.closeConnection();
+    }
+
     public void openConnection(){
         if(!connection) {
             try {
-                con = DriverManager.getConnection(DB_URL, DB_USER, DB_PASS);
-                stmt = con.createStatement();
-                connection = true;
+                con         = DriverManager.getConnection(DB_URL, DB_USER, DB_PASS);
+                stmt        = con.createStatement();
+                connection  = true;
                 System.out.println("Successfully connected");
             } catch (SQLException ex) {
                 ex.printStackTrace();
@@ -30,16 +44,15 @@ public class DB {
     }
 
     public ResultSet query(String query){
-        ResultSet rs;
+        ResultSet rs = null;
         if(connection){
             try {
                 rs = stmt.executeQuery(query);
-                rs.close();
             } catch(SQLException ex) {
                 ex.printStackTrace();
             }
         }
-        return null;
+        return rs;
     }
 
     public void closeConnection(){
