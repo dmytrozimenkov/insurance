a25912bfe7e1e75224f6c3dcad8276e8b5d6d2c6
dmytrozimenkov <dizimka@icloud.com>
2017-05-20 16:16:03 +0300

    1

--- controller/LoginController.java
+++ controller/LoginController.java
@@ -1,11 +1,17 @@
 package sample.controller;
 
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 import sample.DB;
 
+import java.io.IOException;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
@@ -17,23 +23,40 @@ public class LoginController {
     @FXML
     private TextField password;
 
-    public void login_func()  {
+    public void login_func()  throws Exception{
+
         DB db = new DB();
         db.openConnection();
         ResultSet rs = db.query("SELECT id_agent, password FROM Agent WHERE id_agent = '" + login.getText() + "' AND password = '" + password.getText() + "'");
         try{
             if (!rs.isBeforeFirst() ) {
                 System.out.println("No data");
+                Alert alert = new Alert(Alert.AlertType.ERROR);
+                alert.setContentText("Неправильный id/пароль");
+                alert.setHeaderText("Ошибка входа");
+                alert.initStyle(StageStyle.TRANSPARENT);
+                alert.showAndWait();
+
             } else {
+
                 while (rs.next()) {
+                    try {
+                        // AnchorPane pane = (AnchorPane) FXMLLoader.load(getClass().getResource("window.fxml"));
+                        Parent root = FXMLLoader.load(getClass().getResource("../view/window.fxml"));
+                        Stage stage = new Stage();
+                        stage.setScene(new Scene(root, 800, 600));
+                        stage.show();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
                     System.out.println(rs.getInt(1));
                     System.out.println(rs.getString(2));
                 }
             }
-        } catch (SQLException ex){
+        } catch (SQLException ex) {
             ex.printStackTrace();
         }
 
-        db.closeConnection();
+            db.closeConnection();
+        }
     }
-}
---
+++ controller/windowController.java
@@ -0,0 +1,7 @@
+package sample.controller;
+
+/**
+ * Created by dmytrozimenkov on 20.05.17.
+ */
+public class windowController {
+}
