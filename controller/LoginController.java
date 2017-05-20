09d1185259a79f716f582d56b14305b9a658afbe
dmytrozimenkov <dizimka@icloud.com>
2017-05-20 21:11:01 +0300

    1

--- controller/LoginController.java
+++ controller/LoginController.java
@@ -6,6 +6,7 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.ButtonType;
+import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
@@ -24,7 +25,7 @@ public class LoginController {
     @FXML
     private TextField login;
     @FXML
-    private TextField password;
+    private PasswordField password;
 
     public void login_func()  throws Exception{
         DB db = new DB();
